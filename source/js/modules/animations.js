31fa789f16b9868cf4abc03a4eeae347ce329c15
e.bublikova <e.bublikova@cinet.ru>
2022-02-23 21:01:11 +0300

    module1-task3

--- source/js/modules/animations.js
+++ source/js/modules/animations.js
@@ -1,6 +1,14 @@
-export default() => {
-	window.addEventListener('load', function() {
-		document.body.classList.add('loaded');
-		console.log('hi')
-	});
-}
+export default () => {
+  window.addEventListener("load", function () {
+    document.body.classList.add("loaded");
+
+    function backgroundAnimate(id, selector) {
+      if (document.querySelector(id).classList.length === 2) {
+        selector.classList.add("active");
+      }
+    }
+
+    backgroundAnimate("#prizes", background);
+  });
+  const background = document.querySelector(".screen--background");
+};
